5f6e4e089181f5fd4afa74ae8b5721412d03cd14
OwenMcNaughton <owen.mcnaughton@gmail.com>
2017-04-04 17:44:22 +0100

    ampersandgit add .

--- release/crude_multido.py
+++ release/crude_multido.py
@@ -103,7 +103,7 @@ def outerCartIter():
     j += 1
     for i in range(int(iterations)):
       try:
-        p = call(['./main', circfile, str(i), str(threshold), folder])
+        p = call(['./main', circfile, str(i), str(threshold), folder, '&'])
         print("./main " + circfile + " " + str(i) + " " + str(threshold) + " " + folder)
         # output, err = p.communicate()
       except OSError:
